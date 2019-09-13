f1fbba4249d36986f33fececbd027872b4d8e7e1
Imre Angelo <coolimre_@hotmail.com>
2019-09-13 16:48:03 +0200

    Update JoinPage.js

--- src/pages/join/JoinPage.js
+++ src/pages/join/JoinPage.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
 
 export default class JoinPage extends Component {
+    testServer = () => {
+        fetch('http://localhost:3001/join.php', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              name: 'a',
+              mail: 'b',
+            })
+        })
+    }
+
     render() {
         return (
-            <div>
+            <div onClick={() => {
+                this.testServer();
+                console.log("sendt");
+            }}>
                 Join
             </div>
         )
